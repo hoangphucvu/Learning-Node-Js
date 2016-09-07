db6b649e10556f23bb2e5d90f569cff7d2f904cd
Phuc Ngo <ngohungphuc95@gmail.com>
2016-09-07 15:24:49 +0700

    Todo cross page testing - page 68

--- Chap5/index.js
+++ Chap5/index.js
@@ -20,7 +20,6 @@ app.use(function(req, res, next) {
 });
 
 
-
 app.get('/', function(req, res) {
     res.render('home');
 });
@@ -30,6 +29,16 @@ app.get('/about', function(req, res) {
         fortune: fortune.getFortune(),
         pageTestScript: '/qa/tests-about.js'
     });
+});
+
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){
+    res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
 });
 
 // 404 catch-all handler (middleware)
