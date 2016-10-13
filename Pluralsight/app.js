c7f17ade6dbbe0554c34f5ec158f549ad71633b5
hoangphucvu <ngohungphuc95@gmail.com>
2016-10-13 13:47:05 +0700

    Static file

--- Pluralsight/app.js
+++ Pluralsight/app.js
@@ -1,9 +1,17 @@
 /**
  * Created by phuc.ngo on 13/10/2016.
  */
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = 8000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/',function (req,res) {
+    res.send('main');
+});
+
 app.listen(port,function(error){
-    console.log("Running on port " + port);
+    console.log('Running on port ' + port);
 });
