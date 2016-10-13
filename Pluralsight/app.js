08fba25139cab4179ed8bec964e0c146c8d1a4b5
hoangphucvu <ngohungphuc95@gmail.com>
2016-10-13 15:43:26 +0700

    Working with nodemon

--- Pluralsight/app.js
+++ Pluralsight/app.js
@@ -3,7 +3,7 @@
  */
 var express = require('express');
 var app = express();
-var port = 8000;
+var port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
